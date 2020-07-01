9936948cfb4712eeb4808b7aa6d33a2176be2701
Sirius-Coder <eche17056@rgipt.ac.in>
2020-07-01 20:01:01 +0530

    message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 const path = require("path")
 var morgan = require('morgan')
 require("dotenv").config()
-var port = 5000 || process.env.PORT;
+var port = process.env.PORT || 5000;
 var productData;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
