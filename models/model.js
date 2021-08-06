69c3e05713d7d28e7618209be97c68bd29781420
Sirius-Coder <eche17056@rgipt.ac.in>
2021-08-06 17:15:43 +0530

    Major Changes

--- models/model.js
+++ models/model.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pewdiepie', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoose = require('mongoose');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/pewdiepie',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 const db = mongoose.connection;
 db.on('open', () => {
-    console.log('The connection to the Database has been established');
-})
+  console.log('The connection to the Database has been established');
+});
 
-db.on('error', (err) => { console.error(err) })
+db.on('error', (err) => {
+  console.error(err);
+});
 
 //Our Schema
 var cartSchema = new mongoose.Schema({
-    title: String,
-    id: Number,
-    price: Number,
-    img: String
-})
+  title: String,
+  id: Number,
+  price: Number,
+  img: String,
+  size: String,
+});
 
 var model = mongoose.model('model', cartSchema);
 
-module.exports = model
+module.exports = model;
